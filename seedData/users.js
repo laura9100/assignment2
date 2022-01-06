050f4f7b5fe40c4e5184fe8dee0d4a661b1b326a
Laura McBride <20084164@mail.wit.ie>
2022-01-06 13:56:23 +0000

    Start assignments.

---
+++ seedData/users.js
@@ -0,0 +1,12 @@
+const  users = [
+    {
+        'username': 'user1',
+        'password': 'test1',
+    },
+    {
+        'username': 'user2',
+        'password': 'test2',
+    },
+];
+export default users;
+
